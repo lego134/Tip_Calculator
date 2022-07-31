175fcb50478dd7ac93438e71802c32486d30dd81
Jonah Raiford <xiafei44@icloud.com>
2022-07-31 10:53:40 -0600

    Tip Calculator

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+let AmountEl = document.getElementById('Amount');
+let TipEl = document.getElementById('Tip');
+let peopleEl = document.getElementById('people');
+let peopleEl2 = Number(peopleEl.innerText)
+let TotalPerson = document.getElementById('Total_person');
+function Tip(){
+    let Calculation = Number(AmountEl.value) * Number(TipEl.value)/100
+    let food_and_tip = Calculation + Number(AmountEl.value)
+    let divide_people =  food_and_tip / peopleEl2
+    let final_total = divide_people
+   TotalPerson.innerText =  final_total
+} 
+const increasePeople = () => {
+    peopleEl2 += 1
+    peopleEl.innerText = peopleEl2
+  }
+  const decreasePeople = () => {
+    if( peopleEl2 <= 1){
+        let message = alert('We can not calculate at zero, pleas start at one.')
+        return message
+    }
+    peopleEl2 -= 1
+    peopleEl.innerText = peopleEl2
+  }
 
-const Tip = () => {
-    let AmountEl = document.querySelector('#Amount').value;
-    let TipEl = document.querySelector('#Tip').value;
-    let peopleEl = document.querySelector('#people').value;
-    let TotalPerson = document.querySelector('#Total_person');
-    let Calculation = Number(AmountEl) * Number(TipEl)/100
-    let food_and_tip = Calculation + Number(AmountEl)
-    let final =  food_and_tip / Number(peopleEl)
-    let final_total =  final 
-    console.log(TotalPerson.innerText = '$' + final_total)
-}
-document.querySelector('#TipTotal').addEventListener('click', function() {
-    Tip();
-})
